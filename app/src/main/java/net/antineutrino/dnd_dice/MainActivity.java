094a68eb573b09b0b473a4d144fcb41d5bad4737
theJollySin <john_science@protonmail.com>
2020-10-07 07:31:54 -0400

    clean dice app, no functionality yet

--- app/src/main/java/net/antineutrino/dnd_dice/MainActivity.java
+++ app/src/main/java/net/antineutrino/dnd_dice/MainActivity.java
@@ -7,190 +7,94 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    Button button0, button1, button2, button3, button4, button5, button6,
-            button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
-            buttonMul, button10, buttonC, buttonEqual;
+    Button button4, button6, button8, button10, button12, button20, buttonRoll;
     EditText editText;
+    int dieSides = 0;
 
-    float mValueOne, mValueTwo;
-
-    boolean addition, mSubtract, multiplication, division;
+    protected void deClickAll() {
+        button4.setPressed(false);
+        button6.setPressed(false);
+        button8.setPressed(false);
+        button10.setPressed(false);
+        button12.setPressed(false);
+        button20.setPressed(false);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button0 = (Button) findViewById(R.id.button0);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
         button4 = (Button) findViewById(R.id.button4);
-        button5 = (Button) findViewById(R.id.button5);
         button6 = (Button) findViewById(R.id.button6);
-        button7 = (Button) findViewById(R.id.button7);
         button8 = (Button) findViewById(R.id.button8);
-        button9 = (Button) findViewById(R.id.button9);
         button10 = (Button) findViewById(R.id.button10);
-        buttonAdd = (Button) findViewById(R.id.buttonadd);
-        buttonSub = (Button) findViewById(R.id.buttonsub);
-        buttonMul = (Button) findViewById(R.id.buttonmul);
-        buttonDivision = (Button) findViewById(R.id.buttondiv);
-        buttonC = (Button) findViewById(R.id.buttonC);
-        buttonEqual = (Button) findViewById(R.id.buttoneql);
+        button12 = (Button) findViewById(R.id.button12);
+        button20 = (Button) findViewById(R.id.button20);
+        buttonRoll = (Button) findViewById(R.id.buttonRoll);
         editText = (EditText) findViewById(R.id.edt1);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "1");
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "2");
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "3");
-            }
-        });
 
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText() + "4");
-            }
-        });
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "5");
+                deClickAll();
+                button4.setPressed(true);
+                dieSides = 4;
             }
         });
 
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText() + "6");
-            }
-        });
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "7");
+                deClickAll();
+                button6.setPressed(true);
+                dieSides = 6;
             }
         });
 
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText() + "8");
-            }
-        });
-
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "9");
-            }
-        });
-
-        button0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText(editText.getText() + "0");
-            }
-        });
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (editText == null) {
-                    editText.setText("");
-                } else {
-                    mValueOne = Float.parseFloat(editText.getText() + "");
-                    addition = true;
-                    editText.setText(null);
-                }
-            }
-        });
-
-        buttonSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueOne = Float.parseFloat(editText.getText() + "");
-                mSubtract = true;
-                editText.setText(null);
-            }
-        });
-
-        buttonMul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueOne = Float.parseFloat(editText.getText() + "");
-                multiplication = true;
-                editText.setText(null);
-            }
-        });
-
-        buttonDivision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueOne = Float.parseFloat(editText.getText() + "");
-                division = true;
-                editText.setText(null);
-            }
-        });
-
-        buttonEqual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueTwo = Float.parseFloat(editText.getText() + "");
-
-                if (addition) {
-                    editText.setText(mValueOne + mValueTwo + "");
-                    addition = false;
-                }
-
-                if (mSubtract) {
-                    editText.setText(mValueOne - mValueTwo + "");
-                    mSubtract = false;
-                }
-
-                if (multiplication) {
-                    editText.setText(mValueOne * mValueTwo + "");
-                    multiplication = false;
-                }
-
-                if (division) {
-                    editText.setText(mValueOne / mValueTwo + "");
-                    division = false;
-                }
-            }
-        });
-
-        buttonC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setText("");
+                deClickAll();
+                button8.setPressed(true);
+                dieSides = 8;
             }
         });
 
         button10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(editText.getText() + ".");
+                deClickAll();
+                button10.setPressed(true);
+                dieSides = 10;
+            }
+        });
+
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deClickAll();
+                button12.setPressed(true);
+                dieSides = 12;
+            }
+        });
+
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deClickAll();
+                button20.setPressed(true);
+                dieSides = 20;
+            }
+        });
+
+        buttonRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(dieSides + " Boom!");
             }
         });
     }
