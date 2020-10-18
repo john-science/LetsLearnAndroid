651a59da1ea932d1d875d2c84364ee3b995debf4
theJollySin <john_science@protonmail.com>
2020-10-17 20:09:30 -0400

    About page now how working back button

--- app/src/main/java/net/antineutrino/dnd_dice/About.java
+++ app/src/main/java/net/antineutrino/dnd_dice/About.java
@@ -1,5 +1,10 @@
 package net.antineutrino.dnd_dice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -9,5 +14,23 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuHomeFromAbout) {
+            // This code will start the new activity when the settings button is clicked on the bar at the top.
+            Intent intent = new Intent(About.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/net/antineutrino/dnd_dice/MainActivity.java
+++ app/src/main/java/net/antineutrino/dnd_dice/MainActivity.java
@@ -179,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menu_home, menu);
         return true;
     }
 
