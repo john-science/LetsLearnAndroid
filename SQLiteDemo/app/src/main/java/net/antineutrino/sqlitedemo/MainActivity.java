a9b258245f8cf3ee48e5eb70ab4c4f73873c5d50
theJollySin <john_science@protonmail.com>
2020-10-29 15:25:51 -0400

    SQLiteDemo - Creating customer object on button click

--- SQLiteDemo/app/src/main/java/net/antineutrino/sqlitedemo/MainActivity.java
+++ SQLiteDemo/app/src/main/java/net/antineutrino/sqlitedemo/MainActivity.java
@@ -1,13 +1,56 @@
 package net.antineutrino.sqlitedemo;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    // references to all of the buttons and controls on the layout
+    Button btn_add, btn_viewAll;
+    EditText edtName, edtAge;
+    Switch sw_active;
+    ListView lv_customer_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        edtName = findViewById(R.id.edtName);
+        edtAge = findViewById(R.id.edtAge);
+        sw_active = findViewById(R.id.sw_active);
+        lv_customer_list = findViewById(R.id.lv_customerList);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    CustomerModel cust = new CustomerModel(-1, edtName.getText().toString(), Integer.parseInt(edtAge.getText().toString()), sw_active.isChecked());
+                    Toast.makeText(MainActivity.this, cust.toString(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    CustomerModel cust = new CustomerModel(-1, edtName.getText().toString(), Integer.parseInt(edtAge.getText().toString()), sw_active.isChecked());
+                    Toast.makeText(MainActivity.this, cust.toString(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
